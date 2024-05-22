27dbcd0a06bee30886809a61db8a949c382e1ac2
Cristian Pena <cristianpenamesina@gmail.com>
2024-05-22 19:33:23 -0400

    set up project

---
+++ contactos-app-back/src/main/java/com/cpena/contactos/back/ContactosAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.cpena.contactos.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactosAppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactosAppBackApplication.class, args);
+	}
+
+}
---
+++ contactos-app-back/src/test/java/com/cpena/contactos/back/ContactosAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpena.contactos.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactosAppBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
